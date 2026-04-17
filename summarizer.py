b6c9c0326ee3b8ef4bec8cdfd7332e170f6f4992
Uthpala-Perera <uthpalaperera54@gmail.com>
2026-04-17 20:23:05 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,12 +1,8 @@
 from transformers import pipeline
 
-# Load once
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
 def summarize_text(text):
-    # Limit input size (important!)
     text = text[:1000]
-
-    summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
-
+    summary = summarizer(text, max_length=100, min_length=25, do_sample=False)
     return summary[0]["summary_text"]
